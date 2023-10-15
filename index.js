6b5dda4159c1ac370501f4c60fa47ed3401f8ccd
Atri Thakar <atrithaker122@gmail.com>
2023-10-15 19:06:11 +0530

    used Fetch API

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
+let check = undefined
+
 mongoose.connect('mongodb://127.0.0.1:27017/DepositAndWithdraw');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -28,23 +30,28 @@ app.post('/details',async (req, res) => {
     try {
         check = await account.findOne({ holdername: req.body.holdername })
         if (check.holdername === req.body.holdername) {
-            let depo = check.deposit
-            let deposit = depo.toString()
-
-
-            res.send(deposit)
-            console.log(check.deposit)
+            // let depo = check.deposit
+            // let deposit = depo.toString()
+            res.sendFile(__dirname+'/index.html')
+            
+            
+            // console.log(check.deposit)
         }
         else {
             // res.sendFile(__dirname + '/html/login.html')
             res.send('Not Found')
-
+            
         }
     }
     catch {
         res.send("Oops! Something went wrong...")
-
+        
     }
+})
+
+app.get('/api/details',(req,res)=>{
+    
+    res.json(check)
 })
 
 app.listen(port, hostname, () => {
